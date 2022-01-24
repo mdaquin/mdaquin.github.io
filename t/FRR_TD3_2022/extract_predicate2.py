2c981a4c90b8cdfd8ca096d3a1a77ac6dd115809
mdaquin <mathieu.daquin.x@gmail.com>
2022-01-24 17:35:28 +0100

    v2 of py in FRR tD3

---
+++ t/FRR_TD3_2022/extract_predicate2.py
@@ -0,0 +1,24 @@
+import sys
+
+predicate = sys.argv[1]
+
+headers = []
+data = []
+
+with open("mkbodies.csv") as f:
+    line = f.readline()
+    first = True
+    while line:
+        la = line.split(",")
+        if first:
+            headers=la
+            first = False
+        else:
+            data.append(la)
+        line = f.readline()
+        
+def tovar(s):
+    return s.lower().replace(" ", "").replace(".", "").replace("-", "")
+
+for x in data:
+    print(f"{predicate.lower()}({tovar(x[0])}, {x[headers.index(predicate)]}).")
