73da5387906556c21b2821a850e412cc2a108680
mdaquin <mathieu.daquin.x@gmail.com>
2022-02-07 20:43:37 +0100

    added scripts in FRR TD3

---
+++ t/FRR_TD3_2022/extract_comp2.py
@@ -0,0 +1,33 @@
+import sys
+
+predicate = sys.argv[1]
+opred = sys.argv[2]
+
+headers = []
+data = []
+
+with open("mkbodies.csv") as f:
+    line = f.readline()
+    first = True
+    while line:
+        la = line.split(",")
+        if first:
+            headers=la
+            first = False
+        else:
+            data.append(la)
+        line = f.readline()
+        
+def tovar(s):
+    return s.lower().replace(" ", "").replace(".", "").replace("-", "")
+
+
+for x in data:
+    for y in data:
+        if x[headers.index(predicate)] == "high":
+            if y[headers.index(predicate)] == "medium" or y[headers.index(predicate)] == "low":
+                print(f"{opred.lower()}({tovar(x[0])}, {tovar(y[0])}).")
+        if x[headers.index(predicate)] == "medium":
+            if y[headers.index(predicate)] == "low":            
+                print(f"{opred.lower()}({tovar(x[0])}, {tovar(y[0])}).")
+
